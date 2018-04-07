f05d77dab22ccfe43978b5c1d443f52e437e0502
Leonel Oliveira <leoneloliver@gmail.com>
2018-04-07 11:02:37 -0400

    implementing search

--- js/search.js
+++ js/search.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
   setTimeout(function(){ 
-    // search top bar behaviors
+    activateSearch();
+  },3000); 
+});
+
+function activateSearch(){
+  // search top bar behaviors
     $('[data-search]').on('change keyup input', function() {
       var searchVal = $(this).val();
       var filterItems = $('[data-filter-item]');
       if ( searchVal !== '' ) {
-      	filterItems.addClass('hide');
-      	$('[data-filter-item][data-filter-name*="' + searchVal + '"]').removeClass('hide');
-      	//$('.term').text("#"+searchVal.toLowerCase());
+        filterItems.addClass('hide');
+        $('[data-filter-item][data-filter-name*="' + searchVal + '"]').removeClass('hide');
+        //$('.term').text("#"+searchVal.toLowerCase());
       } else {
-      	filterItems.removeClass('hide');
-      	//$('.term').text('');
+        filterItems.removeClass('hide');
+        //$('.term').text('');
       }
     });
-  },3000); 
-});
+}
