ecf1e3231fb0a3b38d096de234176bcc54b0e4b7
Leonel Oliveira <leoneloliver@gmail.com>
2018-04-06 22:34:05 -0400

    creating routers

--- js/script.js
+++ js/script.js
@@ -91,17 +91,23 @@ setTimeout(function(){ loadChartHOme(); }, 1000);
 var showChart = function () {
   var element = document.getElementById("charthome");
   element.classList.remove("hide");
+  loadChartHOme();
 };
 var hideChart = function () {
   var element = document.getElementById("charthome");
   element.classList.add("hide");
-};  
+}; 
 
-document.getElementById("btn-home").addEventListener("click", showChart);
 
-document.getElementById("btn-add").addEventListener("click", hideChart);
+setTimeout(function(){ 
 
-document.getElementById("btn-search").addEventListener("click", hideChart);
+  document.getElementById("btn-home").addEventListener("click", showChart);
+  document.getElementById("btn-add").addEventListener("click", hideChart);
+  document.getElementById("btn-search").addEventListener("click", hideChart);
+
+}, 1000); 
+
+
 
 
 
