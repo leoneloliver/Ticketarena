f3703d202851ba5a0479256079e74b43d8f521a5
Leonel Oliveira <leoneloliver@gmail.com>
2018-04-06 20:49:12 -0400

    creating routers

--- js/home-controller.js
+++ js/home-controller.js
@@ -1,66 +1,58 @@
 
-var app = angular.module("ticketarena", [])
-  .controller("listCtrl", function ($scope, $http) {
+var app = angular.module("ticketarena", ['ngRoute']);
+
+  app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "list.html"
+    })
+    .when("/full", {
+        templateUrl : "full-list.html"
+    })
+  });
+
+  app.controller('listCtrl', ['$scope', '$filter', '$http', function($scope, $filter, $http) {
     $http.get('https://api.myjson.com/bins/1fmg07').
       then(function(response) {
           $scope.concerts = response.data;
       });
+
+      //--------------------------------------------------
+      // working with data storege
+    
+      //save it with local storage
+      if(localStorage.concerts){
+        var Myconcert = localStorage.concerts;
+        // var Myconcert = JSON.stringify(localStorage.concerts);
+      }else{
+        //convert JSON animal into a string
+        var Myconcert = JSON.stringify($scope.concerts);
+      }
+      window.localStorage.setItem('concerts', Myconcert);
       
-      $scope.viewFile = function () {
-        return "template/list.html";
-      };
+      $scope.Newconcerts = JSON.parse(window.localStorage.getItem('concerts'));
+      // console.log($scope.Newconcerts);
+      // console.log('first call');
+      
+      $scope.insertData = function (){
+        // inserting new data
+        var oldArray = JSON.parse(window.localStorage.getItem('concerts'));
+        var newProduct = [{ title: "Halsey", image: "https://ssli.ebayimg.com/images/g/oy4AAOSwsW9Y2tRg/s-l1600.jpg", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", dateEvent: "Thu, Aug 30, 2018 - 7:00pm", location: "Rogers Centre, Toronto ON", price: 300 }];
+  
+        console.log(oldArray.concat(newProduct));
+        //convert JSON animal into a string
+        var ConvertMyconcert = JSON.stringify(oldArray.concat(newProduct));
+        //save it with local storage
+        window.localStorage.setItem('concerts', ConvertMyconcert);
+        
+        $scope.Newconcerts = JSON.parse(window.localStorage.getItem('concerts'));
+      }
+      //----------------------------------------------------
       
       $scope.showAll = function () {
-        $scope.viewFile = function () {
-          return  "template/full-list.html";
-        };
         var element = document.getElementById("showmore");
         element.parentNode.removeChild(element);
       };
-  });
+  }]);
 
-
-// angular.module("ticketarena", [])
-//   .controller("listCtrl", function ($scope) {
-//       $scope.concerts = [
-//           { title: "Halsey", image: "https://ssli.ebayimg.com/images/g/oy4AAOSwsW9Y2tRg/s-l1600.jpg", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", dateEvent: "Thu, Aug 30, 2018 - 7:00pm", location: "Rogers Centre, Toronto ON", price: 300 },
-//           { title: "Ed Sheeran", image: "https://ssli.ebayimg.com/images/g/4l8AAOSw32lYyHi1/s-l1600.jpg", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", dateEvent: "Sat, Aug 30, 2018 - 7:00pm", location: "Rogers Centre, Toronto ON", price: 260 },
-//           { title: "5 Seconds of Summer", image: "https://ssli.ebayimg.com/images/g/Yo8AAOSwfVpYo4aY/s-l1600.jpg", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", dateEvent: "Sat, Aug 30, 2018 - 7:00pm", location: "Rogers Centre, Toronto ON", price: 320 },
-//           { title: "Pink", image: "https://ssli.ebayimg.com/images/g/F~EAAOSwNnRYh-WS/s-l1600.jpg", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", dateEvent: "Thu, Sep 24, 2018 - 7:00pm", location: "Air Canada Centre, Toronto ON", price: 310 },
-//           { title: "Camila Cabello", image: "https://ssli.ebayimg.com/images/g/gjgAAOSwi1ZZciJ2/s-l1600.jpg", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", dateEvent: "Thu, Oct 14, 2018 - 7:00pm", location: "Air Canada Centre, Toronto ON", price: 120 },
-//           { title: "Ozzy Osbourne", image: "https://ssli.ebayimg.com/images/g/mNkAAOSwvKtY~mFO/s-l1600.jpg", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", dateEvent: "Sat, Sep 30, 2018 - 7:00pm", location: "Budweiser Stage, Toronto ON", price: 250 }
-//       ];
-      
-//       // $scope.viewFile = function (page) {
-//       //   return $page "list.html";
-//       // };
-      
-//       $scope.viewFile = function () {
-//         return "template/list.html";
-//       };
-      
-//       $scope.showAll = function () {
-//         $scope.viewFile = function () {
-//           return  "template/full-list.html";
-//         };
-//         var element = document.getElementById("showmore");
-//         element.parentNode.removeChild(element);
-//       };
-
-      
-      
-//   });
-
-
-
-
-// var app = angular.module('ticketarena', []);
-// app.controller('listCtrl', function($scope, $http) {
-//     $http.get('https://api.myjson.com/bins/1fmg07').
-//         then(function(response) {
-//             $scope.concerts = response.data;
-//         });
-// });
-  
-  
 
