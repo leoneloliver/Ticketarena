abd1153c81b37f71931249b0d6b9b55aee5e7b93
Leonel Oliveira <leoneloliver@gmail.com>
2018-04-06 23:35:45 -0400

    bugfixes

--- js/script.js
+++ js/script.js
@@ -104,26 +104,6 @@ setTimeout(function(){
 
 
 
-function ScrollTo(name) {
-  //init thread
-  ScrollToResolver(document.getElementById(name));
-}
-
-function ScrollToResolver(elem) {
-  var jump = parseInt(elem.getBoundingClientRect().top * .2);
-  document.body.scrollTop += jump;
-  document.documentElement.scrollTop += jump;
-  //lastjump detects anchor unreachable, also manual scrolling to cancel animation if scroll > jump
-  if (!elem.lastjump || elem.lastjump > Math.abs(jump)) {
-    elem.lastjump = Math.abs(jump);
-    setTimeout(function() {
-      ScrollToResolver(elem);
-    }, "100");
-  } else {
-    elem.lastjump = null;
-  }
-}
-
 
   
 
