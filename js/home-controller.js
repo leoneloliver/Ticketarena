dfc5c65ca813f4ef6ad2c9b3eb0fd146e64a084e
Leonel Oliveira <leoneloliver@gmail.com>
2018-04-06 23:21:27 -0400

    bugfixes

--- js/home-controller.js
+++ js/home-controller.js
@@ -15,11 +15,11 @@ myApp.config(function($routeProvider) {
     $http.get('https://api.myjson.com/bins/1fmg07').
       then(function(response) {
           $scope.concerts = response.data;
-          console.log(JSON.parse($scope.concerts));
+          // console.log(JSON.parse($scope.concerts));
       });
 
 
-      $scope.concerts = [{"title":"Halsey","image":"https://ssli.ebayimg.com/images/g/oy4AAOSwsW9Y2tRg/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Thu, Aug 30, 2018 - 7:00pm","location":"Rogers Centre, Toronto ON","price":300},{"title":"Ed Sheeran","image":"https://ssli.ebayimg.com/images/g/4l8AAOSw32lYyHi1/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Sat, Aug 30, 2018 - 7:00pm","location":"Rogers Centre, Toronto ON","price":260},{"title":"5 Seconds of Summer","image":"https://ssli.ebayimg.com/images/g/Yo8AAOSwfVpYo4aY/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Sat, Aug 30, 2018 - 7:00pm","location":"Rogers Centre, Toronto ON","price":320},{"title":"Pink","image":"https://ssli.ebayimg.com/images/g/F~EAAOSwNnRYh-WS/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Thu, Sep 24, 2018 - 7:00pm","location":"Air Canada Centre, Toronto ON","price":310},{"title":"Camila Cabello","image":"https://ssli.ebayimg.com/images/g/gjgAAOSwi1ZZciJ2/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Thu, Oct 14, 2018 - 7:00pm","location":"Air Canada Centre, Toronto ON","price":120},{"title":"Ozzy Osbourne","image":"https://ssli.ebayimg.com/images/g/mNkAAOSwvKtY~mFO/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Sat, Sep 30, 2018 - 7:00pm","location":"Budweiser Stage, Toronto ON","price":250}];
+      // $scope.concerts = [{"title":"Halsey","image":"https://ssli.ebayimg.com/images/g/oy4AAOSwsW9Y2tRg/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Thu, Aug 30, 2018 - 7:00pm","location":"Rogers Centre, Toronto ON","price":300},{"title":"Ed Sheeran","image":"https://ssli.ebayimg.com/images/g/4l8AAOSw32lYyHi1/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Sat, Aug 30, 2018 - 7:00pm","location":"Rogers Centre, Toronto ON","price":260},{"title":"5 Seconds of Summer","image":"https://ssli.ebayimg.com/images/g/Yo8AAOSwfVpYo4aY/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Sat, Aug 30, 2018 - 7:00pm","location":"Rogers Centre, Toronto ON","price":320},{"title":"Pink","image":"https://ssli.ebayimg.com/images/g/F~EAAOSwNnRYh-WS/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Thu, Sep 24, 2018 - 7:00pm","location":"Air Canada Centre, Toronto ON","price":310},{"title":"Camila Cabello","image":"https://ssli.ebayimg.com/images/g/gjgAAOSwi1ZZciJ2/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Thu, Oct 14, 2018 - 7:00pm","location":"Air Canada Centre, Toronto ON","price":120},{"title":"Ozzy Osbourne","image":"https://ssli.ebayimg.com/images/g/mNkAAOSwvKtY~mFO/s-l1600.jpg","desc":"Lorem Ipsum is simply dummy text of the printing and typesetting industry.","dateEvent":"Sat, Sep 30, 2018 - 7:00pm","location":"Budweiser Stage, Toronto ON","price":250}];
       
       //--------------------------------------------------
       // working with data storege
@@ -32,7 +32,8 @@ myApp.config(function($routeProvider) {
       }
       window.localStorage.setItem('concerts', Myconcert);
       
-      $scope.Newconcerts = JSON.parse(window.localStorage.getItem('concerts'));
+      // $scope.Newconcerts = JSON.parse(window.localStorage.getItem('concerts'));
+      $scope.Newconcerts = window.localStorage.getItem('concerts');
       
       $scope.insertData = function (){
         // inserting new data
@@ -42,7 +43,8 @@ myApp.config(function($routeProvider) {
         var ConvertMyconcert = JSON.stringify(oldArray.concat(newProduct));
         window.localStorage.setItem('concerts', ConvertMyconcert);
         
-        $scope.Newconcerts = JSON.parse(window.localStorage.getItem('concerts'));
+        // $scope.Newconcerts = JSON.parse(window.localStorage.getItem('concerts'));
+        $scope.Newconcerts = window.localStorage.getItem('concerts');
       }
       //----------------------------------------------------
      
