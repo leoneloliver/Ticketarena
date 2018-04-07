49e0df12c0ee0a6b7ccddeb75d6931e93197be19
Leonel Oliveira <leoneloliver@gmail.com>
2018-04-06 23:13:46 -0400

    bugfixes

--- js/home-controller.js
+++ js/home-controller.js
@@ -35,7 +35,8 @@ myApp.config(function($routeProvider) {
       if(localStorage.concerts){
         var Myconcert = localStorage.concerts;
       }else{
-        var Myconcert = JSON.stringify($scope.concerts);
+        // var Myconcert = JSON.stringify($scope.concerts);
+        var Myconcert = $scope.concerts;
       }
       window.localStorage.setItem('concerts', Myconcert);
       
