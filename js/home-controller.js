53f402e9076aaa3bbcd766ce5fa9ca41fb72f639
Leonel Oliveira <leoneloliver@gmail.com>
2018-04-06 21:26:37 -0400

    creating routers

--- js/home-controller.js
+++ js/home-controller.js
@@ -1,6 +1,5 @@
 var myApp = angular.module("ticketarena", ['ngRoute']);
 
-
 myApp.config(function($routeProvider) {
     $routeProvider
     .when("/", {
@@ -18,7 +17,6 @@ myApp.config(function($routeProvider) {
           $scope.concerts = response.data;
       });
 
-
       // $scope.concerts = [
       //     { title: "Halsey", image: "https://ssli.ebayimg.com/images/g/oy4AAOSwsW9Y2tRg/s-l1600.jpg", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", dateEvent: "Thu, Aug 30, 2018 - 7:00pm", location: "Rogers Centre, Toronto ON", price: 300 },
       //     { title: "Ed Sheeran", image: "https://ssli.ebayimg.com/images/g/4l8AAOSw32lYyHi1/s-l1600.jpg", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", dateEvent: "Sat, Aug 30, 2018 - 7:00pm", location: "Rogers Centre, Toronto ON", price: 260 },
@@ -35,57 +33,31 @@ myApp.config(function($routeProvider) {
       if(localStorage.concerts){
         var Myconcert = localStorage.concerts;
       }else{
-        //convert JSON animal into a string
         var Myconcert = $scope.concerts;
       }
       window.localStorage.setItem('concerts', Myconcert);
       
       $scope.Newconcerts = JSON.parse(window.localStorage.getItem('concerts'));
-      // console.log($scope.Newconcerts);
-      // console.log('first call');
       
       $scope.insertData = function (){
         // inserting new data
         var oldArray = JSON.parse(window.localStorage.getItem('concerts'));
         var newProduct = [{ title: "Halsey", image: "https://ssli.ebayimg.com/images/g/oy4AAOSwsW9Y2tRg/s-l1600.jpg", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", dateEvent: "Thu, Aug 30, 2018 - 7:00pm", location: "Rogers Centre, Toronto ON", price: 300 }];
   
-        console.log(oldArray.concat(newProduct));
-        //convert JSON animal into a string
         var ConvertMyconcert = JSON.stringify(oldArray.concat(newProduct));
-        //save it with local storage
         window.localStorage.setItem('concerts', ConvertMyconcert);
         
         $scope.Newconcerts = JSON.parse(window.localStorage.getItem('concerts'));
       }
       //----------------------------------------------------
-      
-      // $scope.viewFile = function (page) {
-      //   return $page "list.html";
-      // };
-      
-      $scope.viewFile = function () {
-        return "list.html";
-      };
-      
+     
       $scope.showAll = function () {
-        $scope.viewFile = function () {
-          return  "full-list.html";
-        };
         var element = document.getElementById("showmore");
         element.parentNode.removeChild(element);
-      };
-
-      
+      };      
       
   }]);
   
-// var app = angular.module('ticketarena', []);
-// app.controller('listCtrl', function($scope, $http) {
-//     $http.get('https://api.myjson.com/bins/1fmg07').
-//         then(function(response) {
-//             $scope.concerts = response.data;
-//         });
-// });
   
   
 
