53fca49729446dd0a95b318206a524ec32480fd7
Leonel Oliveira <leoneloliver@gmail.com>
2018-04-07 20:24:39 -0400

    new page

--- js/search.js
+++ js/search.js
@@ -30,7 +30,7 @@ var myarr = myUrl.split("!");
 //Then read the values from the array where 0 is the first
 console.log('first page router: '+myarr[1]);
 
-if((myarr[1]=='/search' || myarr[1]=='/new-product'){
+if((myarr[1]=='/search') || (myarr[1]=='/new-product')){
   hideChart();
 }
 
